09f2f420ecb55ba1d86625dc8eaa099e600abf50
Yash-123patel <patelyash250702@gmail.com>
2024-12-24 14:15:22 +0530

    Leaned about typegaurds

---
+++ TypeGaurds/demo.ts
@@ -0,0 +1,21 @@
+// deno-lint-ignore-file
+ 
+ 
+//Type of
+//primitive data types like = (String,number,boolean,symbol,undefined,Object)
+ 
+const name1:any="Hello";
+ 
+if(typeof name1==="string"){  
+    console.log(name1.toUpperCase());
+}
+ 
+//Instance of
+class Animal {}
+class Dog extends Animal {}
+ 
+let pet = new Dog();
+ 
+console.log(pet instanceof Dog); // true
+console.log(pet instanceof Animal); // true
+ 
